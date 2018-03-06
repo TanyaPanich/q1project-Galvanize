91b03b11e7d7e608681468079b8a100ccb11f072
Tanya <mellene@gmail.com>
2018-03-06 13:38:07 -0700

    Fixed indentation

--- js/main.js
+++ js/main.js
@@ -1,47 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const renderHeroes = function(heroesArr) {
-      // if (!localStorage.getItem('heroes')) {
-      //   localStorage.setItem('heroes', JSON.stringify(heroesArr))
-      // }
-      // //? What this line does ??
-      // const readyHeroes = JSON.parse(localStorage.getItem('heroes'))
-      console.log('ready to render')
+  getHeroesAndRender()
 
-      for(let hero of heroesArr) {
-        hero.name = heroesArr.name
-        hero.imageSrc = heroesArr.thumbnail.path + "/portrait_fantastic." + heroesArr.thumbnail.extension
-        hero.description = heroesArr.description
-        //heroes.push(oneHero)
-        const $cols12m3 = $('<div>').addClass('col s12 m3')
-        const $card = $('<div>').addClass('card')
-        const $cardImage = $('<div>').addClass('card-image')
-        const $img = $('<img>')
-        $img.attr({
-          src: hero.imageSrc
-        })
-        const $span = $('<span>').addClass('card-title')
-        $span.text(hero.name)
-
-        localStorage.setItem('heroes', JSON.stringify(readyHeroes))
-
-        $cardImage.append($img, $span)
-        $card.append($cardImage)
-        $cols12m3.append($card)
-        const $listings = $('#listings')
-        $listings.append($cols12m3)
-      }
-
+  function renderHeroesCards(heroesArr) {
+    for(let hero of heroesArr) {
+      const $cols10m3 = $('<div>').addClass('col s10 m3')
+      const $card = $('<div>').addClass('card')
+      const $cardImage = $('<div>').addClass('card-image')
+      const $img = $('<img>')
+      $img.attr({
+        src: hero.imageSrc
+      })
+      const $span = $('<span>').addClass('card-title')
+      $span.text(hero.name)
+      $cardImage.append($img, $span)
+      $card.append($cardImage)
+      $cols10m3.append($card)
+      $('#listings').append($cols10m3)
     }
-    function getHeroesAndRender() {
+  }
+
+  function getHeroesAndRender() {
+    if (localStorage.getItem('heroes')) {
+      renderHeroesCards(JSON.parse(localStorage.getItem('heroes')))
+    } else {
+      console.log('>>> Going into the API')
       const heroesURL = [
         `https://gateway.marvel.com:443/v1/public/characters?name=Spider-Man&`,
-        `https://gateway.marvel.com:443/v1/public/characters?name=Wolverine&`,
+        //`https://gateway.marvel.com:443/v1/public/characters?name=Wolverine&`,
         `https://gateway.marvel.com:443/v1/public/characters?name=Hulk&`,
         //`https://gateway.marvel.com:443/v1/public/characters?name=Captain%20America&`,
         `https://gateway.marvel.com:443/v1/public/characters?name=Iron%20Man&`,
-        `https://gateway.marvel.com:443/v1/public/characters?name=Thor&`,
-        `https://gateway.marvel.com:443/v1/public/characters?name=Daredevil&`
+        `https://gateway.marvel.com:443/v1/public/characters?name=Thor&`
+        //`https://gateway.marvel.com:443/v1/public/characters?name=Daredevil&`
       ]
       const ts = new Date().getTime()
       const myPublic = '9345b62919a166acccb31ff15d68d7b8'
@@ -49,30 +40,32 @@ document.addEventListener('DOMContentLoaded', function () {
       const hash = MD5(ts + myPrivate + myPublic).toString()
       let count = 0
       let allHeroes = []
-      for(let basicUrl of heroesURL){
+      for (let basicUrl of heroesURL) {
         let url = basicUrl + 'ts=' + ts + '&apikey=9345b62919a166acccb31ff15d68d7b8&hash=' + hash
         let $xhr = $.getJSON(url)
         $xhr.done(function(input){
-          console.log('request is done')
-          let heroRes = input.data.results[0]
-          allHeroes.push(heroRes);
 
-          count++
-          console.log('count is: ' + count)
+          let heroRes = input.data.results[0]
+          let hero = {}
+
+          hero.name = heroRes.name
+          hero.imageSrc = heroRes.thumbnail.path + "/portrait_fantastic." + heroRes.thumbnail.extension
+          hero.description = heroRes.description
+          //hero.id =heroRes.id
+          allHeroes.push(hero)
         })
       }
-      console.log('ALLHEROES', allHeroes);
       setTimeout(function() {localStorage.setItem('heroes', JSON.stringify(allHeroes))}, 1000)
-      setTimeout(function() {renderHeroes(allHeroes)}, 1000)
+      setTimeout(function() {renderHeroesCards(allHeroes)}, 1000)
     }
-
-    // if(localStorage.getItem('heroes')){
-    //   renderHeroes(JSON.parse(localStorage.getItem('heroes')))
-    // } else {
-    //   getHeroesAndRender()
-    // }
-    getHeroesAndRender()
+  }
 })
+
+// $('.card').on('click', function(event) {
+//   console.log("i am here");
+//   let spanText = $(this).children().classList('card-title').text
+//   console.log(spanText);
+// })
 
 // module.exports = {
 //   renderHeroes: renderHeroes
