8bf11ca0d7bf54c8f95d6785731ff676c4d1d57a
Tanya <mellene@gmail.com>
2018-03-06 22:35:56 -0700

    render marvel api, store in local storage, render from it, added dragula

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   getHeroesAndRender()
+  dragula([document.querySelector('#heroContainer'), document.querySelector('#taskContainer')]);
 
   function renderHeroesCards(heroesArr) {
     for(let hero of heroesArr) {
       const $cols10m3 = $('<div>').addClass('col s10 m3')
       const $card = $('<div>').addClass('card')
+      const $a = $('<a>')
+      $a.attr({href:'2_2.html'})
+
+      //$card.on('click', heroSelectClick)
+
+      $card.attr('data-hero-name', hero.name)
+      $card.attr('data-hero-image-url', hero.imageSrc)
+      $card.attr('data-hero-description', hero.description)
+
       const $cardImage = $('<div>').addClass('card-image')
       const $img = $('<img>')
       $img.attr({
@@ -15,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
       $span.text(hero.name)
       $cardImage.append($img, $span)
       $card.append($cardImage)
-      $cols10m3.append($card)
+      $a.append($card)
+      $cols10m3.append($a)
       $('#listings').append($cols10m3)
     }
   }
@@ -44,6 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let url = basicUrl + 'ts=' + ts + '&apikey=9345b62919a166acccb31ff15d68d7b8&hash=' + hash
         let $xhr = $.getJSON(url)
         $xhr.done(function(input){
+          console.log('callback');
 
           let heroRes = input.data.results[0]
           let hero = {}
@@ -51,21 +63,51 @@ document.addEventListener('DOMContentLoaded', function () {
           hero.name = heroRes.name
           hero.imageSrc = heroRes.thumbnail.path + "/portrait_fantastic." + heroRes.thumbnail.extension
           hero.description = heroRes.description
-          //hero.id =heroRes.id
           allHeroes.push(hero)
+          if(allHeroes.length === heroesURL.length){
+            localStorage.setItem('heroes', JSON.stringify(allHeroes))
+            renderHeroesCards(allHeroes)
+          }
         })
       }
-      setTimeout(function() {localStorage.setItem('heroes', JSON.stringify(allHeroes))}, 1000)
-      setTimeout(function() {renderHeroesCards(allHeroes)}, 1000)
     }
   }
-})
+  function renderFavoriteHero(name, url, description) {
+    const $cols12m7 = $('<div>').addClass('col s12 m12')
+    const $header = $('<h2>').addClass('header')
+    $header.text('Hey, ' + name + "! What you need to accomlish right now?")
+    const $cardHorizontal = $('<div>').addClass('card horizontal')
+    const $cardImage = $('<div>').addClass('card-image')
+    const $img = $('<img>')
+    $img.attr({
+      src: url
+    })
+    const $cardStacked = $('<div>').addClass('card-stacked')
+    const $cardContent = $('<div>').addClass('card-content')
+    const $p = $('<p>')
+    $p.text(description)
+    $cardContent.append($p)
+    $cardStacked.append($cardContent)
+    $cardImage.append($img)
+    $cardHorizontal.append($cardImage, $cardStacked)
+    $cols12m7.append($header, $cardHorizontal)
+    $('#listings').append($cols12m7)
+  }
 
-// $('.card').on('click', function(event) {
-//   console.log("i am here");
-//   let spanText = $(this).children().classList('card-title').text
-//   console.log(spanText);
-// })
+  function heroSelectClick(event) {
+    event.preventDefault()
+    const heroName = $(this).attr('data-hero-name')
+    const heroImageUrl = $(this).attr('data-hero-image-url')
+    const heroDescription = $(this).attr('data-hero-description')
+
+    $('#listings').empty()
+    $('#steps').text('Step 2: Choose your goals!')
+    renderFavoriteHero(heroName, heroImageUrl, heroDescription)
+    $('#taskContainer').css('display','block')
+
+  }
+
+})
 
 // module.exports = {
 //   renderHeroes: renderHeroes
